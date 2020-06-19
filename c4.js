c5f94900c9cddd1c52d066c7ae6d29955b989b45
Matheus Santana <embs@cin.ufpe.br>
2020-06-18 21:32:48 -0300

    Allow two players place circles with different colors
    Missing the check on whether the game has come to an end.

--- c4.js
+++ c4.js
@@ -1,7 +1,33 @@
+function occupy(el, playerTurn) {
+  var col = el.target.parentElement;
+  //console.log(col);
+  //debugger
+  var freeCell = col.querySelectorAll(".column_cell.free");
+  if(freeCell.length == 0) {
+    return
+  }
+  var toBeOccupied = freeCell[freeCell.length-1];
+  //console.log(freeCell);
+  toBeOccupied.classList.remove('free');
+  toBeOccupied.classList.add('occupied');
+  var span = document.createElement("span");
+  span.classList.add(`circle`);
+  span.classList.add(`circle-player${playerTurn}`);
+  toBeOccupied.appendChild(span);
+}
+
+var playerTurn = '1';
+
 window.addEventListener('DOMContentLoaded', (event) => {
   document.querySelectorAll('.column').forEach(function(column) {
     column.addEventListener('click', function(el) {
-      alert('click');
+      occupy(el, playerTurn);
+
+      if(playerTurn === '1') {
+        playerTurn = '2';
+      } else {
+        playerTurn = '1';
+      }
     });
   });
 });
